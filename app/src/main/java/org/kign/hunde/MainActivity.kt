dbd31e745017132454e215098fb8a1b27f9c8a47
Balazs Lengyak <balazs@lengyak.hu>
2021-09-21 16:54:55 +0200

    replaced fragment to FragmentContainerView in content_main.xml

--- app/src/main/java/org/kign/hunde/MainActivity.kt
+++ app/src/main/java/org/kign/hunde/MainActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -14,6 +16,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,7 +26,10 @@ class MainActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.toolbar)
 
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_content_main) as NavHostFragment
+        navController = navHostFragment.navController
+        //val navController = findNavController(R.id.nav_host_fragment_content_main)
         appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
     }
@@ -47,7 +53,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun onAboutItemSelected(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
         navController.navigate(R.id.aboutFragment)
         return true
     }
